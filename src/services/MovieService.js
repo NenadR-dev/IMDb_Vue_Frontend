54939ed296fd21bfd32ba931e7dfb4f00aec581e
Nenad Radulovic <popcornsftw2011@hotmail.com>
2020-12-16 15:41:18 +0100

    #PIT7-14 Create Movie Page

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {url} from './url.js'
 import {parseError} from './ErrorParser.js'
+import {getToken} from './AuthService.js'
 export const getMovies = () => {
     return axios.get(`${url}/movies`)
         .then(response => {
@@ -23,6 +24,26 @@ export const getMovieByID = id => {
 
 export const getNextMoviePage = (url) => {
     return axios.get(url)
+        .then(response => {
+            return response.data
+        })
+        .catch(err => {
+            throw parseError(err.response.data.errors)
+        })
+}
+
+export const addMovie = (data) => {
+    let movieData = new FormData()
+    movieData.append('imageCover',data.imageCover)
+    movieData.append('title',data.title)
+    movieData.append('description',data.description),
+    movieData.append('genre', data.genre)
+    
+    return axios.post(`${url}/movies`,movieData,{
+        headers: {
+            'Authorization': `Bearer ${getToken()}`
+        }
+    })
         .then(response => {
             return response.data
         })
